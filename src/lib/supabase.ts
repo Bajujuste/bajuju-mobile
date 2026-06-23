ea37b240ea0eb4d92f6f880657a3be5b4bf04397
Bajujuste <info@Bajuju.it>
2026-06-23 10:47:26 +0000

    Add Supabase client

---
+++ src/lib/supabase.ts
@@ -0,0 +1,16 @@
+import 'react-native-url-polyfill/auto';
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://xwcbmsfsirggozpcskcz.supabase.co';
+const supabaseAnonKey = 'sb_publishable_Kg74KvC--sJpim0_tY7K0Q_YJcMqf9g';
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: AsyncStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});

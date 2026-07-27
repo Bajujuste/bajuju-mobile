903d0532107652997963064341e66caa575eb3b0
Bajujuste <info@Bajuju.it>
2026-07-27 11:48:56 +0200

    Bundle icon fonts with Bajuju preview

--- scripts/prepare-preview-fonts.sh
+++ scripts/prepare-preview-fonts.sh
@@ -5,4 +5,6 @@ base64 -d .preview-fonts/Fredoka-400.ttf.b64 > assets/fonts/Fredoka-400.ttf
 base64 -d .preview-fonts/Fredoka-500.ttf.b64 > assets/fonts/Fredoka-500.ttf
 base64 -d .preview-fonts/Fredoka-600.ttf.b64 > assets/fonts/Fredoka-600.ttf
 base64 -d .preview-fonts/Fredoka-700.ttf.b64 > assets/fonts/Fredoka-700.ttf
+cp node_modules/@expo/vector-icons/build/vendor/react-native-vector-icons/Fonts/Ionicons.ttf assets/fonts/Ionicons.ttf
+cp node_modules/@expo/vector-icons/build/vendor/react-native-vector-icons/Fonts/MaterialCommunityIcons.ttf assets/fonts/MaterialCommunityIcons.ttf
 echo "Font Bajuju pronti."

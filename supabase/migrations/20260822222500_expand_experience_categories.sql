095b5b09e120c2bb634b5e654bd83b663b639029
Bajujuste <info@Bajuju.it>
2026-08-23 00:05:20 +0200

    DB: distingue cultura musica e giochi

---
+++ supabase/migrations/20260822222500_expand_experience_categories.sql
@@ -0,0 +1,3 @@
+alter type public.activity_category add value if not exists 'cultura';
+alter type public.activity_category add value if not exists 'musica';
+alter type public.activity_category add value if not exists 'giochi';

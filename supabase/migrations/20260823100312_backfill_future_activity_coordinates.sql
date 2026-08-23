1db422fddf3043206b56c03c248ad317ac84eb58
Bajujuste <info@Bajuju.it>
2026-08-23 12:03:37 +0200

    DB: completa coordinate eventi futuri legacy

---
+++ supabase/migrations/20260823100312_backfill_future_activity_coordinates.sql
@@ -0,0 +1,9 @@
+update public.activities
+set latitude = 45.8342441, longitude = 9.9024052
+where id = 'e93b0f76-b3fc-49aa-a7d8-1ae9340ec65e'
+  and latitude is null and longitude is null;
+
+update public.activities
+set latitude = 45.8736403, longitude = 9.8891249
+where id = '203ca8dc-9794-4c11-a1f0-af451b8e90f7'
+  and latitude is null and longitude is null;
